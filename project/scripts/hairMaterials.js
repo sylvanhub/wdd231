3ed4cbf7a6612264702c7d5f7d9cd5b31ab8ea6d
sylvanhub <sylvesterodeh9@gmail.com>
2024-10-27 05:15:09 +0100

    path update

--- project/scripts/hairMaterials.js
+++ project/scripts/hairMaterials.js
@@ -3,7 +3,7 @@
 // Function to fetch hair materials from JSON file
 async function fetchhairMaterials() {
     try {
-        const response = await fetch('C:\Users\SYLVESTER\Documents\byu C1\third semester\WDD231(WEB FRONTEND1)\wdd231\project\data\activities.json'); // Update the path as needed
+        const response = await fetch('https://github.com/sylvanhub/wdd231/blob/main/project/data/activities.json'); // Update the path as needed
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
